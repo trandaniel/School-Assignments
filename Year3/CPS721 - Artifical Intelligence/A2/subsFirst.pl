3ce83dc8363f449d78f5170e456fd3052f5c9dbe
Daniel Tran <tran.daniel95@gmail.com>
2017-01-21 23:44:41 -0500

    Update subsFirst.pl

--- Year3/CPS721 - Artifical Intelligence/A2/subsFirst.pl
+++ Year3/CPS721 - Artifical Intelligence/A2/subsFirst.pl
@@ -1,9 +1,3 @@
-%Student, Section, CSID, SID
-%Daniel Tran, 02, d45tran, 500554290
-%Samuel Dindyal, 03, s2dindya, 500592528
-%Balin Banh, 03, b2banh, 500574970
-
-
 memberTree(X,tree(X,Left,Mid,Right)).
 memberTree(X,tree(Y,Left,Mid,Right)) :- memberTree(X,Left).
 memberTree(X,tree(Y,Left,Mid,Right)) :- memberTree(X,Mid).
