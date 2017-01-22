1d0636149ace7393f243c79a55665501b05f9c5f
Daniel Tran <tran.daniel95@gmail.com>
2017-01-21 23:45:35 -0500

    Update grade.pl

--- Year3/CPS721 - Artifical Intelligence/A1/grade.pl
+++ Year3/CPS721 - Artifical Intelligence/A1/grade.pl
@@ -1,8 +1,3 @@
-%Student, Section, CSID, SID
-%Daniel Tran, 02, d45tran, 500554290
-%Samuel Dindyal, 03, s2dindya, 500592528
-%Balin Banh, 03, b2banh, 500574970
-
 assignment(1, 60) .
 assignment(2, 70) .
 assignment(3, 85) .
